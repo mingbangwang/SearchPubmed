9783ee801cfeaf070ecb5f75dbbf737be709ca1e
mingbangwang <mingbang.wang.bgi@qq.com>
2021-03-16 14:29:43 +0800

    新增文件

--- search_pubmed.py
+++ search_pubmed.py
@@ -97,170 +97,171 @@ print("journal_list:", journal_list)
 keyword_terms = ["deep learning", "machine learning", "neural network"]
 print("keyword_terms:", keyword_terms)
 
-keyword_term = keyword_terms[0]  # 可以设置循环
-print("pubmed for keyword_term:", keyword_term)
-keyword_term_save = keyword_term.replace(" ", "_")
+# keyword_term = keyword_terms[0]  # 可以设置循环
+for keyword_term in keyword_terms:
+    print("pubmed for keyword_term:", keyword_term)
+    keyword_term_save = keyword_term.replace(" ", "_")
 
-# 建立保存原始keyword_term pubmed原始结果的文件夹
-keyword_term_dir = os.path.join(out_dir, keyword_term_save)
-keyword_term_rawdata = os.path.join(keyword_term_dir, "pubmed_tsv")
+    # 建立保存原始keyword_term pubmed原始结果的文件夹
+    keyword_term_dir = os.path.join(out_dir, keyword_term_save)
+    keyword_term_rawdata = os.path.join(keyword_term_dir, "pubmed_tsv")
 
-os.makedirs(keyword_term_rawdata, mode=0o777, exist_ok=True)
+    os.makedirs(keyword_term_rawdata, mode=0o777, exist_ok=True)
 
-## 开始进行pubmed查找
-# 针对指定的keyword_term查询每一个杂志
-# issn_term = issn_terms[1] # 可以设置循环
-for issn_term in issn_terms:
-# print(issn)
-    print("searching {} in {}".format(keyword_term,issn_journal_dict[issn_term]))
-    # 设置keyword term
-    # term = "deep learning and {}".format(issn) # 2041-1723,nature communication等
-    term = keyword_term + " and " + issn_term
-    term_list = term.split(" ")
-    term_issn_save = keyword_term_save + "_" + issn_journal_dict[issn_term]
-    # term_issn_save
+    ## 开始进行pubmed查找
+    # 针对指定的keyword_term查询每一个杂志
+    # issn_term = issn_terms[1] # 可以设置循环
+    for issn_term in issn_terms:
+    # print(issn)
+        print("searching {} in {}".format(keyword_term,issn_journal_dict[issn_term]))
+        # 设置keyword term
+        # term = "deep learning and {}".format(issn) # 2041-1723,nature communication等
+        term = keyword_term + " and " + issn_term
+        term_list = term.split(" ")
+        term_issn_save = keyword_term_save + "_" + issn_journal_dict[issn_term]
+        # term_issn_save
 
-    # search pubmed
-    # 如果已经跑过这个杂志，就跳过
-    out_file = os.path.join(keyword_term_dir, "pubmed_out_{}.tsv".format(term_issn_save))
-    if os.path.exists(out_file):
-        print("already done: searching for {} in {}".format(keyword_term,issn_journal_dict[issn_term]))
-        continue
+        # search pubmed
+        # 如果已经跑过这个杂志，就跳过
+        out_file = os.path.join(keyword_term_rawdata, "pubmed_out_{}.tsv".format(term_issn_save))
+        if os.path.exists(out_file):
+            print("already done: searching for {} in {}".format(keyword_term,issn_journal_dict[issn_term]))
+            continue
 
-    # 开始跑pubmed
-    Entrez.email = 'mingbang.wang.bgi@qq.com'  # always tell who you are
-    handle = Entrez.egquery(term=term)
+        # 开始跑pubmed
+        Entrez.email = 'mingbang.wang.bgi@qq.com'  # always tell who you are
+        handle = Entrez.egquery(term=term)
 
-    record = Entrez.read(handle)
-    for row in record["eGQueryResult"]:
-        if row["DbName"] == "pubmed":
-            print("count:", row["Count"])
+        record = Entrez.read(handle)
+        for row in record["eGQueryResult"]:
+            if row["DbName"] == "pubmed":
+                print("count:", row["Count"])
 
-    handle = Entrez.esearch(db="pubmed", term=term, retmax=500)
-    record = Entrez.read(handle)
-    idlist = record["IdList"]
-    # print (idlist[0:40]) #取前40个看下输出的信息是什么
+        handle = Entrez.esearch(db="pubmed", term=term, retmax=500)
+        record = Entrez.read(handle)
+        idlist = record["IdList"]
+        # print (idlist[0:40]) #取前40个看下输出的信息是什么
 
-    # id = idlist[0:10] #取前10个测试一下文章标题和作者信息
-    id = idlist
-    print("records counts:", len(id))
+        # id = idlist[0:10] #取前10个测试一下文章标题和作者信息
+        id = idlist
+        print("records counts:", len(id))
 
-    # 如果没有查到结果，跳到下一个循环
-    if len(id) == 0:
-        print("none count: searching for {} in {}".format(keyword_term,issn_journal_dict[issn_term]))
-        continue
+        # 如果没有查到结果，跳到下一个循环
+        if len(id) == 0:
+            print("none count: searching for {} in {}".format(keyword_term,issn_journal_dict[issn_term]))
+            continue
 
-    fout = open(out_file, 'w')
-    fout.write("Authors\tTitle\tJournal\tYear\tPublication_citation\tAbstract\tKeywords\tDate_epublished\tPMID\n")
+        fout = open(out_file, 'w')
+        fout.write("Authors\tTitle\tJournal\tYear\tPublication_citation\tAbstract\tKeywords\tDate_epublished\tPMID\n")
 
-    handle = Entrez.efetch(db="pubmed", id=id, rettype="medline", retmode="text")
-    records = Medline.parse(handle)
-    records = list(records)  # records 是一个迭代器，所以只能访问这些records一次。如果想保存这些records，需要把他们转成列表。
-    journal_list = []
-    abstract_list = []
-    keyword_list = []
-    for record in records:
-        Authors_full_name = "; ".join(record.get("FAU", "?"))
-        Authors_simplified_name = "; ".join(record.get("AU", "?"))
-        Publication_title = record.get("TI", "?")
-        Publication_citation = record.get("SO", "?")
-        Journal_Title_simplified = record.get("TA", "?")
-        Journal_Title = record.get("JT", "?")
-        Abstract = record.get("AB", "?")
+        handle = Entrez.efetch(db="pubmed", id=id, rettype="medline", retmode="text")
+        records = Medline.parse(handle)
+        records = list(records)  # records 是一个迭代器，所以只能访问这些records一次。如果想保存这些records，需要把他们转成列表。
+        journal_list = []
+        abstract_list = []
+        keyword_list = []
+        for record in records:
+            Authors_full_name = "; ".join(record.get("FAU", "?"))
+            Authors_simplified_name = "; ".join(record.get("AU", "?"))
+            Publication_title = record.get("TI", "?")
+            Publication_citation = record.get("SO", "?")
+            Journal_Title_simplified = record.get("TA", "?")
+            Journal_Title = record.get("JT", "?")
+            Abstract = record.get("AB", "?")
 
-        # for Keywords
-        Keywords_ = record.get("OT", "?")
-        # remove "*" in Keywords_
-        Keywords = []
-        for word in Keywords_:
-            Keywords.append(word.replace("*", ""))
-        if Keywords == ['?']:
-            Keywords = ""
-        else:
-            Keywords = "; ".join(Keywords)
+            # for Keywords
+            Keywords_ = record.get("OT", "?")
+            # remove "*" in Keywords_
+            Keywords = []
+            for word in Keywords_:
+                Keywords.append(word.replace("*", ""))
+            if Keywords == ['?']:
+                Keywords = ""
+            else:
+                Keywords = "; ".join(Keywords)
 
-        PMID = "https://pubmed.ncbi.nlm.nih.gov/" + record.get("PMID", "?")
-        Date_epublished = record.get("DP", "?")
-        Year_epublished = Date_epublished.split(" ")[0]
-        Date_epublished_num = record.get("DEP", "?")
-        ISSN = record.get("IS", "?")
-        Library_ID = record.get("LID", "?")
+            PMID = "https://pubmed.ncbi.nlm.nih.gov/" + record.get("PMID", "?")
+            Date_epublished = record.get("DP", "?")
+            Year_epublished = Date_epublished.split(" ")[0]
+            Date_epublished_num = record.get("DEP", "?")
+            ISSN = record.get("IS", "?")
+            Library_ID = record.get("LID", "?")
 
-        # for word_cloud
-        journal_list.append(Journal_Title_simplified)
-        abstract_list.append(Abstract)
-        keyword_list.append(Keywords)
+            # for word_cloud
+            journal_list.append(Journal_Title_simplified)
+            abstract_list.append(Abstract)
+            keyword_list.append(Keywords)
 
-        fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(Authors_full_name, Publication_title, Journal_Title_simplified, Year_epublished, Publication_citation, Abstract, Keywords, Date_epublished_num, PMID))
-    fout.close()
+            fout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(Authors_full_name, Publication_title, Journal_Title_simplified, Year_epublished, Publication_citation, Abstract, Keywords, Date_epublished_num, PMID))
+        fout.close()
 
 
-## 结果统计
-# 调用函数原始的df
-df = dataframe_dir_to_df(keyword_term_rawdata)
-# 只保留research paper
-df = df[df['Abstract'] != '?']  # 可以修改为其他条件
-# 保存df
-df.to_csv(os.path.join(keyword_term_dir, "{}_summary.tsv".format(keyword_term_save)), sep="\t", index=False)
+    ## 结果统计
+    # 调用函数原始的df
+    df = dataframe_dir_to_df(keyword_term_rawdata)
+    # 只保留research paper
+    df = df[df['Abstract'] != '?']  # 可以修改为其他条件
+    # 保存df
+    df.to_csv(os.path.join(keyword_term_dir, "{}_summary.tsv".format(keyword_term_save)), sep="\t", index=False)
 
-# 按照杂志进行统计
-result = pd.value_counts(df['Journal'])
-# type(result)
-df_summary = pd.DataFrame(columns=['Journal', 'Count'])
-df_summary['Count'] = result
-df_summary["Journal"] = df_summary.index
-# df_summary
-df_summary = df_summary[df_summary["Count"] > 1]  #
-# df_summary
-plt.figure(figsize=(10, 12))  #
-ax = sns.barplot(y=df_summary.Journal, x=df_summary.Count)  #
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=10)  #
-plt.title('Pubmed term:\n{}'.format(keyword_term), fontsize=14)  # keyword_term 替换project_id
-plt.xlabel("Pubmed records count", fontsize=14)
-plt.ylabel("Journal", fontsize=14)
+    # 按照杂志进行统计
+    result = pd.value_counts(df['Journal'])
+    # type(result)
+    df_summary = pd.DataFrame(columns=['Journal', 'Count'])
+    df_summary['Count'] = result
+    df_summary["Journal"] = df_summary.index
+    # df_summary
+    df_summary = df_summary[df_summary["Count"] > 1]  #
+    # df_summary
+    plt.figure(figsize=(10, 12))  #
+    ax = sns.barplot(y=df_summary.Journal, x=df_summary.Count)  #
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=10)  #
+    plt.title('Pubmed term:\n{}'.format(keyword_term), fontsize=14)  # keyword_term 替换project_id
+    plt.xlabel("Pubmed records count", fontsize=14)
+    plt.ylabel("Journal", fontsize=14)
 
-plt.show()
-fig = ax.get_figure()
-fig.savefig(keyword_term_dir + "/barplot_{}_journal.pdf".format(keyword_term_save), dpi=400, bbox_inches='tight')
-# df_summary.to_csv("{}_{}_summary.tsv".format(keyword_term,'Journal'),sep="\t",index=False)
+    plt.show()
+    fig = ax.get_figure()
+    fig.savefig(keyword_term_dir + "/barplot_{}_journal.pdf".format(keyword_term_save), dpi=400, bbox_inches='tight')
+    # df_summary.to_csv("{}_{}_summary.tsv".format(keyword_term,'Journal'),sep="\t",index=False)
 
-# 按照年份进行统计
-result = pd.value_counts(df['Year'])
-# type(result)
-df_summary = pd.DataFrame(columns=['Year', 'Count'])
-df_summary['Count'] = result
-df_summary["Year"] = df_summary.index
-# df_summary
-df_summary = df_summary[df_summary["Count"] > 1]  #
-# df_summary
-plt.figure(figsize=(10, 12))  #
-ax = sns.barplot(x=df_summary.Year, y=df_summary.Count)  # ,orient='h'
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=10)  #
-plt.title('Pubmed term:\n{}'.format(keyword_term), fontsize=14)  # keyword_term 替换project_id
-plt.ylabel("Pubmed records count", fontsize=14)
-plt.xlabel("Year", fontsize=14)
-# 展示图片
-plt.show()
-# 保存图片
-fig = ax.get_figure()
-fig.savefig(keyword_term_dir + "/barplot_{}_year.pdf".format(keyword_term_save), dpi=400, bbox_inches='tight')
-# df_summary.to_csv("{}_{}_summary.tsv".format(keyword_term,'Journal'),sep="\t",index=False)
+    # 按照年份进行统计
+    result = pd.value_counts(df['Year'])
+    # type(result)
+    df_summary = pd.DataFrame(columns=['Year', 'Count'])
+    df_summary['Count'] = result
+    df_summary["Year"] = df_summary.index
+    # df_summary
+    df_summary = df_summary[df_summary["Count"] > 1]  #
+    # df_summary
+    plt.figure(figsize=(10, 12))  #
+    ax = sns.barplot(x=df_summary.Year, y=df_summary.Count)  # ,orient='h'
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=10)  #
+    plt.title('Pubmed term:\n{}'.format(keyword_term), fontsize=14)  # keyword_term 替换project_id
+    plt.ylabel("Pubmed records count", fontsize=14)
+    plt.xlabel("Year", fontsize=14)
+    # 展示图片
+    plt.show()
+    # 保存图片
+    fig = ax.get_figure()
+    fig.savefig(keyword_term_dir + "/barplot_{}_year.pdf".format(keyword_term_save), dpi=400, bbox_inches='tight')
+    # df_summary.to_csv("{}_{}_summary.tsv".format(keyword_term,'Journal'),sep="\t",index=False)
 
-## 词云图
-df = pd.read_csv(os.path.join(keyword_term_dir, "{}_summary.tsv".format(keyword_term_save)), sep="\t")
-wordcloud_list = ["Title", "Abstract", "Keywords"]  # 有时候提取不出来
-for name in wordcloud_list:
-    # remove empty text
-    text_list = df[name].to_list()
-    text_list_filtered = []
-    for text in text_list:
-        if str(text) != 'nan':
-            text_list_filtered.append(str(text))
-    # print(text_list_filtered)
-    # word cloud
-    text = ' '.join(text_list_filtered)
-    wc = WordCloud(width=1200, height=800)
-    wc.generate(text)
-    wc.to_file(keyword_term_dir + "/wordcloud_{}_{}.pdf".format(keyword_term_save, name))
+    ## 词云图
+    df = pd.read_csv(os.path.join(keyword_term_dir, "{}_summary.tsv".format(keyword_term_save)), sep="\t")
+    wordcloud_list = ["Title", "Abstract", "Keywords"]  # 有时候提取不出来
+    for name in wordcloud_list:
+        # remove empty text
+        text_list = df[name].to_list()
+        text_list_filtered = []
+        for text in text_list:
+            if str(text) != 'nan':
+                text_list_filtered.append(str(text))
+        # print(text_list_filtered)
+        # word cloud
+        text = ' '.join(text_list_filtered)
+        wc = WordCloud(width=1200, height=800)
+        wc.generate(text)
+        wc.to_file(keyword_term_dir + "/wordcloud_{}_{}.pdf".format(keyword_term_save, name))
